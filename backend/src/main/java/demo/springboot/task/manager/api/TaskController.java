11f46243f12f86c7f7216e663a237c3c0da8f481
Or Bartal <orbartal@gmail.com>
2023-07-10 00:01:46 -0400

    Remove unused annotation CrossOrigin.

--- backend/src/main/java/demo/springboot/task/manager/api/TaskController.java
+++ backend/src/main/java/demo/springboot/task/manager/api/TaskController.java
@@ -4,17 +4,15 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 import demo.springboot.task.manager.service.TaskService;
 
-
-@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/task")
 public class TaskController {
--- backend/src/main/java/demo/springboot/task/manager/api/TimeController.java
+++ backend/src/main/java/demo/springboot/task/manager/api/TimeController.java
@@ -3,15 +3,14 @@ package demo.springboot.task.manager.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import demo.springboot.task.manager.model.TimeTaskRequest;
 import demo.springboot.task.manager.service.TimeService;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/time")
 public class TimeController {
