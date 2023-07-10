a308522f7cb8a1560abbd76c7fe8656f1794ad4f
Or Bartal <orbartal@gmail.com>
2023-07-09 23:22:43 -0400

    Init springboot app

---
+++ backend/src/main/java/demo/springboot/task/manager/TaskManagerMain.java
@@ -0,0 +1,13 @@
+package demo.springboot.task.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerMain.class, args);
+	}
+
+}
